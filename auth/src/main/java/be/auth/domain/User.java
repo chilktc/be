5b05324950d36fb733d916eb55a6fa83774610a5
gichan222 <repitor15@gmail.com>
2026-02-03 16:23:01 +0900

    :sparkles: [#1] 회원 정보 저장을 위한 User 도메인 추가

---
+++ auth/src/main/java/be/auth/domain/User.java
@@ -0,0 +1,42 @@
+package be.auth.domain;
+
+import java.util.UUID;
+
+import be.auth.jwt.Role;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Builder
+public class User {
+
+	@Id
+	@Column(nullable = false, updatable = false)
+	private UUID id;
+
+	@Column
+	private String loginId;
+
+	@Column
+	private String password;
+
+	@Column
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	public User(UUID id, String loginId, String password, Role role){
+		this.id = id;
+		this.loginId = loginId;
+		this.password = password;
+		this.role = role;
+	}
+}
