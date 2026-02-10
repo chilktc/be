9c340705311d5e11244a5a103bf2801c1d0b0ff0
minseo <114375668+oneokiwa@users.noreply.github.com>
2026-02-10 12:07:37 +0900

    ✨ [#5] User 도메인 수정

--- auth/src/main/java/be/auth/domain/User.java
+++ auth/src/main/java/be/auth/domain/User.java
@@ -8,25 +8,39 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Builder
+@Table(
+	name = "user",
+	uniqueConstraints = {
+		@UniqueConstraint(
+			columnNames = {"provider", "provider_user_id"}
+		)
+	}
+)
 public class User {
-
 	@Id
 	@Column(nullable = false, updatable = false)
 	private UUID id;
 
-	@Column(nullable = false, unique = true)
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private OauthProvider provider;
+
+	@Column(name = "provider_user_id", nullable = false)
+	private String providerUserId;
+
+	@Column(unique = true)
 	private String loginId;
 
-	@Column(nullable = false)
+	@Column
 	private String password;
 
 	@Column(nullable = false)
@@ -34,10 +48,43 @@ public class User {
 	private Role role;
 
 
-	public User(UUID id, String loginId, String password, Role role){
+	private User(UUID id, OauthProvider provider, String providerUserId, String loginId, String password, Role role){
 		this.id = id;
+		this.provider = provider;
+		this.providerUserId = providerUserId;
 		this.loginId = loginId;
 		this.password = password;
 		this.role = role;
+	}
+
+	public static User createGoogleUser(
+		UUID id,
+		String googleSub,
+		Role role
+	){
+		return new User(
+			id,
+			OauthProvider.GOOGLE,
+			googleSub,
+			null,
+			null,
+			role
+		);
+	}
+
+	public static User createServerUser(
+		UUID id,
+		String loginId,
+		String encodedPassword,
+		Role role
+	){
+		return new User(
+			id,
+			OauthProvider.SERVER,
+			loginId,
+			loginId,
+			encodedPassword,
+			role
+		);
 	}
 }
--- auth/src/main/java/be/auth/repository/UserRepository.java
+++ auth/src/main/java/be/auth/repository/UserRepository.java
@@ -5,14 +5,29 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import be.auth.domain.OauthProvider;
 import be.auth.domain.User;
 import be.common.api.CustomException;
 import be.common.api.ErrorCode;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-	Optional<User> findByLoginId(String loginId);
+	Optional<User> findByProviderAndProviderUserId(
+		OauthProvider provider,
+		String providerUserId
+	);
+
+	Optional<User> findByLoginIdAndProvider(
+		String loginId,
+		OauthProvider provider
+	);
+
+	boolean existsByLoginIdAndProvider(
+		String loginId,
+		OauthProvider provider
+	);
+
 	default User findByIdOrThrow(UUID id, ErrorCode errorCode) {
 		return findById(id).orElseThrow(() -> new CustomException(errorCode));
 	}
-	boolean existsByLoginId(String loginId);
+
 }
