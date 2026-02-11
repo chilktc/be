8a7aab2bd2c1b5937cccead43a00c611a388a64d
minseo <114375668+oneokiwa@users.noreply.github.com>
2026-02-11 11:26:14 +0900

    ✨ [#5] 에러코드 추가

--- auth/src/main/java/be/common/api/ErrorCode.java
+++ auth/src/main/java/be/common/api/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
 	EXIST_LOGINID(HttpStatus.BAD_REQUEST, "이미 존재하는 아이디입니다."),
 	EXIST_USER(HttpStatus.BAD_REQUEST, "이미 존재하는 회원입니다."),
 	FAIL_LOGIN(HttpStatus.BAD_REQUEST, "아이디 혹은 비밀번호가 일치하지 않습니다."),
+	INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호를 입력하세요."),
 	DOES_NOT_MATCH_OLD_PASSWORD(HttpStatus.BAD_REQUEST, "기존 비밀번호가 일치하지 않습니다."),
 	CAN_NOT_ALLOWED_SAME_PASSWORD(HttpStatus.BAD_REQUEST, "기존 비밀번호와 동일한 비밀번호로 변경할 수 없습니다."),
 	ACCOUNT_INACTIVATED(HttpStatus.BAD_REQUEST, "비활성화 된 계정입니다."),
