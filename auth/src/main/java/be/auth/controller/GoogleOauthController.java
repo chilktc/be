df67eb6883e388bd5a1598d1709a6be41e2b7030
minseo <114375668+oneokiwa@users.noreply.github.com>
2026-02-12 17:38:00 +0900

    ♻️[#9] 안 쓰는 import 제거

--- auth/src/main/java/be/auth/controller/GoogleOauthController.java
+++ auth/src/main/java/be/auth/controller/GoogleOauthController.java
@@ -1,14 +1,12 @@
 package be.auth.controller;
 
 import java.time.Duration;
-import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
