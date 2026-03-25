8ef10a2d6f2f5098708dbd33e4c2cacdbe6e7db7
minseo <114375668+oneokiwa@users.noreply.github.com>
2026-03-25 16:18:32 +0900

    ♻️ [#53] 게이트웨이 cors 에러 해결 (#54)
    * ✨ 게이트웨이 CORS 설정 추가
    
    * ✨ 게이트웨이 CORS 설정 추가
    
    * ✨ 프론트 테스트 환경을 위한 refreshToken 처리 변경
    
    * ✨ depth 수정

--- auth/src/main/java/be/auth/controller/AuthController.java
+++ auth/src/main/java/be/auth/controller/AuthController.java
@@ -61,8 +61,8 @@ public class AuthController {
 				.httpOnly(true)
 				// TODO : 배포 서비스에서는 true를 사용
 				.secure(true)          // HTTPS 환경에서만
-				.sameSite("Lax")
-				.path("/auth/refresh")
+				.sameSite("None")
+				.path("/")
 				.maxAge(Duration.ofDays(14))
 				.build()
 				.toString()
@@ -92,9 +92,9 @@ public class AuthController {
 			"Set-Cookie",
 			ResponseCookie.from("refreshToken", result.refreshToken())
 				.httpOnly(true)
-				.secure(false)
-				.sameSite("Lax")
-				.path("/auth/refresh")
+				.secure(true)
+				.sameSite("None")
+				.path("/")
 				.maxAge(Duration.ofDays(14))
 				.build()
 				.toString()
@@ -140,9 +140,9 @@ public class AuthController {
 			"Set-Cookie",
 			ResponseCookie.from("refreshToken", "")
 				.httpOnly(true)
-				.secure(false)
-				.sameSite("Lax")
-				.path("/auth/refresh")
+				.secure(true)
+				.sameSite("None")
+				.path("/")
 				.maxAge(0)
 				.build()
 				.toString()
