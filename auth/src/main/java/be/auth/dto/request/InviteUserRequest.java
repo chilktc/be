e2158a1159046b0502a543923892e915dd8d9e85
gichan222 <repitor15@gmail.com>
2026-04-02 10:33:39 +0900

    ✨ [#70] InviteUserRequest 필드 확장

--- auth/src/main/java/be/auth/dto/request/InviteUserRequest.java
+++ auth/src/main/java/be/auth/dto/request/InviteUserRequest.java
@@ -1,9 +1,19 @@
 package be.auth.dto.request;
 
 import be.auth.jwt.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record InviteUserRequest(
+	@NotBlank
+	String name,
+	@NotBlank
 	String email,
+	@NotBlank
+	String department,
+	@NotBlank
+	String position,
+	@NotNull
 	Role role
 )
 {}
