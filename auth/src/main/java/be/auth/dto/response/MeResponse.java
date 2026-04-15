554042fda29fc243ef7ae89084d1dd83693152d0
Kichan Kim <repitor15@gmail.com>
2026-04-15 09:14:58 +0900

    :sparkles: [#122] /auth/me에 role 추가 (#123)

--- auth/src/main/java/be/auth/dto/response/MeResponse.java
+++ auth/src/main/java/be/auth/dto/response/MeResponse.java
@@ -2,10 +2,13 @@ package be.auth.dto.response;
 
 import java.util.UUID;
 
+import be.auth.jwt.Role;
+
 public record MeResponse(
 	UUID id,
 	String email,
 	String nickname,
-	boolean firstLogin
+	boolean firstLogin,
+	Role role
 ) {
 }
